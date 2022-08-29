6fb4dc7ffa7fad64fe6a3c2545dfcacc44aaf862
MHFNaN <88169836+MHFNaN@users.noreply.github.com>
2022-08-29 18:33:47 -0400

    Update Suicide.java

--- src/main/java/me/earth/phobos/features/modules/monke/Suicide.java
+++ src/main/java/me/earth/phobos/features/modules/monke/Suicide.java
@@ -7,22 +7,22 @@ import net.minecraft.network.play.client.CPacketPlayer;
 /**
  * @author ligmaballz
  */
-public class Monke
+public class Suicide
         extends Module {
-    private static Cul cul;
+    private static Suicide suicide;
 
-    public Cul() {
+    public Suicide() {
         super("suicide", "die without the /kill or /suicide", Category.MONKE, true, false, false);
     }
 
     @Override
     public void onEnable() {
-        if (Cul.fullNullCheck()) {
+        if (Suicide.fullNullCheck()) {
             return;
         }
-        mc.getConnection().sendPacket((Packet)new CPacketPlayer.Position(Cul.mc.player.posX, Cul.mc.player.posY, Cul.mc.player.posZ, false));
-        Cul.mc.player.setLocationAndAngles(Cul.mc.player.posX, Cul.mc.player.posY, Cul.mc.player.posZ, Cul.mc.player.rotationYaw, Cul.mc.player.rotationPitch);
-        mc.getConnection().sendPacket((Packet)new CPacketPlayer.Position(Cul.mc.player.posX, Cul.mc.player.posY - 1339.2, Cul.mc.player.posZ, true));
+        mc.getConnection().sendPacket((Packet)new CPacketPlayer.Position(Suicide.mc.player.posX, Suicide.mc.player.posY, Suicide.mc.player.posZ, false));
+        Suicide.mc.player.setLocationAndAngles(Suicide.mc.player.posX, Suicide.mc.player.posY, Suicide.mc.player.posZ, Suicide.mc.player.rotationYaw, Suicide.mc.player.rotationPitch);
+        mc.getConnection().sendPacket((Packet)new CPacketPlayer.Position(Suicide.mc.player.posX, Suicide.mc.player.posY - 1339.2, Suicide.mc.player.posZ, true));
         this.disable();
     }
 }

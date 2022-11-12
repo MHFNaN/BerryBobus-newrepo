7c120da96347254e39a76dec79ffb8da8e1b9cee
MHFNaN <88169836+MHFNaN@users.noreply.github.com>
2022-11-12 09:49:51 -0800

    Update PpESP.java

--- src/main/java/me/earth/phobos/features/modules/render/PpESP.java
+++ src/main/java/me/earth/phobos/features/modules/render/PpESP.java
@@ -5,7 +5,7 @@ import me.earth.phobos.features.modules.Module;
 public class PpESP extends Module {
 
     public PpESP() {
-        super("PenisESP", "shows your small penis", Category.RENDER, true, false, false);
+        super("TestModule", "shows your small penis", Category.RENDER, true, false, false);
     }
     // I'll add this 12/28
 }

580bbeaa3465a756cf6280fbdc7e4805e32e13dd
MHFNaN <88169836+MHFNaN@users.noreply.github.com>
2022-11-11 21:13:59 -0800

    Update ModuleManager.java

--- src/main/java/me/earth/phobos/manager/ModuleManager.java
+++ src/main/java/me/earth/phobos/manager/ModuleManager.java
@@ -192,7 +192,7 @@ public class ModuleManager
         //this.modules.add(new AntiPopLag());
         this.modules.add(new OffhandTolonEdition());
         this.modules.add(new UnicodeOverloader());
-        this.modules.add(new PopCounter());
+        //this.modules.add(new ());
         this.modules.add(new ModuleTools());
         //this.modules.add(new ());
         //this.modules.add(new ());
